d95a5b7216a4237921439dd696ac3e70bfa8da26
Victor <victor.ananias8@gmail.com>
2018-09-21 20:12:07 -0300

    aula 4 ...

--- test/reminders.spec.js
+++ test/reminders.spec.js
@@ -1,10 +1,14 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import expect from 'expect';
 import Reminders from '../src/components/Reminders.vue'
 
 describe ('Reminders.vue', () => {
     it ('hides the reminders list if there are none', () => {
-        let wrapper = shallowMount(Reminders);
+        let wrapper = mount(Reminders);
         expect(wrapper.contains('ul')).toBe(false);
+    });
+
+    it('can add reminders', () => {
+
     });
 });

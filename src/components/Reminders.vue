e24fa4200d55f1a4ac64fea87f882250ac38c547
Victor <victor.ananias8@gmail.com>
2018-09-22 10:22:27 -0300

    aula 4 - DONE

--- src/components/Reminders.vue
+++ src/components/Reminders.vue
@@ -1,8 +1,31 @@
 <template>
-    <div></div>
+    <div>
+        <input type="text" class="new-reminder" v-model="newReminder">
+        <button @click='add'>Add Reminder</button>
+        <ul v-if="reminders.length > 0">
+            <li v-for="reminder in reminders" v-bind:key="reminder">
+                {{ reminder }}
+                <button class="remove" @click="remove(reminder)">Remove</button>
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            reminders: [],
+            newReminder: ''
+        }
+    },
+    methods: {
+        add() {
+            this.reminders.push(this.newReminder);
+            this.newReminder = '';
+        },
+        remove(reminder) {
+            this.reminders = this.reminders.filter(r => r !== reminder);
+        }
+    }
 }
 </script>

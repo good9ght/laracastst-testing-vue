7605bba886b578f6ee6e0ff7b2387f61ab894683
Victor <victor.ananias8@gmail.com>
2018-09-23 09:09:07 -0300

    aula 5 - iniciando

---
+++ test/Question.spec.js
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils';
+import expect from 'expect';
+import Question from '../src/components/Question.vue';
+
+describe('Question', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = mount(Question);
+    });
+});

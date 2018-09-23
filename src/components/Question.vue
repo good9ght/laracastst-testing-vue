f0ee590d09cc009cf408e7d07e3306b538e218a1
Victor <victor.ananias8@gmail.com>
2018-09-23 11:31:17 -0300

    aula 7 - DONE

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,8 +1,44 @@
 <template>
     <div>
+        <div>
+            <!-- Viewing the question. -->
+            <div v-if="!editing">
+                <h1 v-text="title" id="title"></h1>
+                <div v-text="body" id="body"></div>
+                <button id="edit" @click="editing = true">Edit</button>
+            </div>
+            <div v-else>
+                <input type="text" name='title' v-model='form.title'>
+                <textarea name="body" id="body" v-model="form.body"></textarea>
+                <button @click="cancel" id="cancel">Cancel</button>
+                <button @click="update" id="update">Update</button>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
+    props: [ 'question' ],
+    data() {
+        return {
+            title: this.question.title,
+            body: this.question.body,
+            form: {
+                title: this.question.title,
+                body: this.question.body
+            },
+            editing: false
+        }
+    },
+    methods: {
+        cancel() {
+            this.editing = false;
+        },
+        update() {
+            this.title = this.form.title
+            this.body = this.form.body
+            this.editing = false;
+        }
+    }
 }
 </script>
--- test/Question.spec.js
+++ test/Question.spec.js
@@ -4,8 +4,74 @@ import Question from '../src/components/Question.vue';
 
 describe('Question', () => {
     let wrapper;
-
     beforeEach(() => {
-        wrapper = mount(Question);
+        wrapper = mount(Question, {
+            propsData: {
+                question: {
+                    title: 'The Title',
+                    body: 'The Body',
+                }
+            }
+        });
     });
+
+    it ('presets the title and the body', () => {
+        see('The Title');
+        see('The Body');
+    });
+
+    it ('can be edited', () => {
+        expect(wrapper.contains('input[name=title]')).toBe(false);
+        
+        click('#edit');
+
+        expect(wrapper.find('input[name=title]').element.value).toBe('The Title');
+        expect(wrapper.find('textarea[name=body]').element.value).toBe('The Body');
+    });
+
+    it ('hides the edit button during edit mode', () => {
+        expect(wrapper.contains('#edit')).toBe(true);
+
+        click('#edit');
+
+        expect(wrapper.contains('#edit')).toBe(false);
+    });
+
+    it ('updates the the question after being edited', () => {
+        click('#edit');
+
+        type('Changed Title', 'input[name=title]');
+        type('Changed Body', 'textarea[name=body]');
+
+        click('#update');
+
+        see('Changed Title');
+        see('Changed Body');
+    });
+    
+    it ('can cancel out of edit mode', () => {
+        click('#edit');
+        
+        type('Changed Title', 'input[name=title]');
+
+        click('#cancel');
+
+        see('The Title');
+    });
+
+    let see = (text, selector) => {
+        let wrap = selector ? wrapper.find(selector) : wrapper;
+        expect(wrap.html()).toContain(text);
+    }
+    
+    let type = (text, selector) => {
+        let node = wrapper.find(selector);
+        node.element.value = text;
+        node.trigger('input');
+    }
+    
+    let click = (selector) => {
+        wrapper.find(selector).trigger('click');
+    }
+
 });
